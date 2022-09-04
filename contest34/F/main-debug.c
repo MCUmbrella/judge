d038d1486e53145d30b6725e41e2c9998ad8266b
MCUmbrella <mcumbrella@protonmail.com>
2022-09-04 20:53:32 +0800

    e

--- contest34/F/main-debug.c
+++ contest34/F/main-debug.c
@@ -9,6 +9,17 @@ int mode=0;
 int** map;
 int width;
 
+void printMap()
+{
+    if(0) return;
+    for(int i=0; i!=width; i++)
+    {
+        for(int j=0; j!=width; j++)
+            printf("%d ", map[i][j]);
+        puts("");
+    }
+    puts("");
+}
 void color(int width, int line, int col, int mode)
 {
     switch(mode%4)
@@ -17,13 +28,16 @@ void color(int width, int line, int col, int mode)
         {       // GW
             for(int i=0; i!=width/2; i++) // RED
                 for(int j=0; j!=width/2; j++)
-                    map[i+line][j+col] = RED;
+                    {map[i+line][j+col] = RED;printMap();}
+
             for(int i=0; i!=width/2; i++) // BLUE
                 for(int j=0; j!=width/2; j++)
-                    map[i+line][j+col+width/2] = BLUE;
+                    {map[i+line][j+col+width/2] = BLUE;printMap();}
+
             for(int i=0; i!=width/2; i++) // GREEN
                 for(int j=0; j!=width/2; j++)
-                    map[i+line+width/2][j+col] = GREEN;
+                    {map[i+line+width/2][j+col] = GREEN;printMap();}
+
             if(width/2 != 1) color(width/2, line+width/2, col+width/2, ++mode); // WHITE
             break;
         }
@@ -31,13 +45,16 @@ void color(int width, int line, int col, int mode)
         {       // WB
             for(int i=0; i!=width/2; i++) // RED
                 for(int j=0; j!=width/2; j++)
-                    map[i+line][j+col+width/2] = RED;
+                    {map[i+line][j+col+width/2] = RED;printMap();}
+
             for(int i=0; i!=width/2; i++) // BLUE
                 for(int j=0; j!=width/2; j++)
-                    map[i+line+width/2][j+col+width/2] = BLUE;
+                    {map[i+line+width/2][j+col+width/2] = BLUE;printMap();}
+
             for(int i=0; i!=width/2; i++) // GREEN
                 for(int j=0; j!=width/2; j++)
-                    map[i+line][j+col] = GREEN;
+                    {map[i+line][j+col] = GREEN;printMap();}
+
             if(width/2 != 1) color(width/2, line+width/2, col, ++mode); // WHITE
             break;
         }
@@ -45,14 +62,16 @@ void color(int width, int line, int col, int mode)
         {       // BR
             for(int i=0; i!=width/2; i++) // RED
                 for(int j=0; j!=width/2; j++)
-                    map[i+line+width/2][j+col+width/2] = RED;
+                    {map[i+line+width/2][j+col+width/2] = RED;printMap();}
 
             for(int i=0; i!=width/2; i++) // BLUE
                 for(int j=0; j!=width/2; j++)
-                    map[i+line+width/2][j+col] = BLUE;
+                    {map[i+line+width/2][j+col] = BLUE;printMap();}
+
             for(int i=0; i!=width/2; i++) // GREEN
                 for(int j=0; j!=width/2; j++)
-                    map[i+line][j+col+width/2] = GREEN;
+                    {map[i+line][j+col+width/2] = GREEN;printMap();}
+
             if(width/2 != 1) color(width/2, line, col, ++mode); // WHITE
             break;
         }
@@ -60,13 +79,16 @@ void color(int width, int line, int col, int mode)
         {       // RG
             for(int i=0; i!=width/2; i++) // RED
                 for(int j=0; j!=width/2; j++)
-                    map[i+line+width/2][j+col] = RED;
+                    {map[i+line+width/2][j+col] = RED;printMap();}
+
             for(int i=0; i!=width/2; i++) // BLUE
                 for(int j=0; j!=width/2; j++)
-                    map[i+line][j+col] = BLUE;
+                    {map[i+line][j+col] = BLUE;printMap();}
+
             for(int i=0; i!=width/2; i++) // GREEN
                 for(int j=0; j!=width/2; j++)
-                    map[i+line+width/2][j+col+width/2] = GREEN;
+                    {map[i+line+width/2][j+col+width/2] = GREEN;printMap();}
+
             if(width/2 != 1) color(width/2, line, col+width/2, ++mode); // WHITE
             break;
         }
@@ -76,14 +98,17 @@ void color(int width, int line, int col, int mode)
 int main(int argc, char** argv)
 {
     scanf("%d", &width);
+
     map=(int**)malloc(sizeof(int)*width*width); // map = new int[width][width];
     for(int i=0; i!=width; i++)
         map[i] = (int*)malloc(sizeof(int) * width);
+
     color(width, 0, 0, 0);
+
     for(int i=0; i!=width; i++)
     {
         for(int j=0; j!=width; j++)
-            printf("%d", map[i][j]);
+            printf("%d ", map[i][j]);
         puts("");
     }
     return 0;
