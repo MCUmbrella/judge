c479ed6706a9a0a555bbf0f914fd43692f944e47
MCUmbrella <mcumbrella@protonmail.com>
2022-09-11 12:11:12 +0800

    Update main.c

--- contest35/A/main.c
+++ contest35/A/main.c
@@ -1,6 +1,38 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char in[16384];
+char out[16384];
+
 int main(int argc, char** argv)
 {
-    //TODO
+    char curChr=0;
+    char curCnt=1;
+
+    scanf("%16383[^\n]", in);
+
+    curChr=in[0];
+    for(size_t i=0; i!=strlen(in); i++)
+    {
+        if(in[i] == ' ')
+        {
+            strcat(out, "-");
+            curCnt=1;
+            curChr=in[i+1];
+            continue;
+        }
+        if(in[i+1] == in[i]) curCnt++;
+        else
+        {
+            char s[16];
+            sprintf(s, "%d%c", curCnt, curChr);
+            strcat(out, s);
+            curChr=in[i+1];
+            curCnt=1;
+        }
+    }
+
+    printf("%s\n", out);
     return 0;
 }
