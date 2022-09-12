3f480d4620ac09757dce013d30d3bfa184044b2c
MCUmbrella <mcumbrella@protonmail.com>
2022-09-12 22:33:59 +0800

    E

--- contest35/E/main.c
+++ contest35/E/main.c
@@ -1,6 +1,47 @@
 #include<stdio.h>
-int main(int argc, char** argv)
+#include<stdlib.h>
+
+void solve(int ***ptr_nums, int n)
 {
-    //TODO
+    int nt=n;
+    int lines=0;
+    int s=0;
+    for(;;)
+    {
+        if((nt = nt-lines) <= 0) break;
+        lines++;
+    }
+    *ptr_nums = (int**)malloc(sizeof(int*)*lines); // assign lines
+    for(int i=0; i!=lines; i++) // assign line elements and initialize
+    {
+        (*ptr_nums)[i] = (int*)malloc(sizeof(int)*(i+1));
+        for(int j=0; j!=i+1; j++)
+            (*ptr_nums)[i][j] = s < n ? ++s : -1;
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    int n;
+    scanf("%d", &n);
+    int **nums;
+    solve(&nums, n);
+    int size;
+    int s = 0;
+    for (size = 1; s < n; size++)
+    {
+        s += size;
+    }
+    size -= 1;
+    for (int i = 1; i <= size; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("%d", nums[i - 1][j]);
+        }
+        printf("\n");
+        free(nums[i - 1]);
+    }
+    free(nums);
     return 0;
 }
