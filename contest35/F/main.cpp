196523da358ae6dc3dd9973a252f3f36bc63ce39
MCUmbrella <mcumbrella@protonmail.com>
2022-09-14 17:13:10 +0800

    Update main.cpp

--- contest35/F/main.cpp
+++ contest35/F/main.cpp
@@ -1,74 +1,56 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-class record
-{    
-    public:
-    string id;
-    int weight;
-
-    record(string id, int weight)
-    {
-        this->id = id;
-        this->weight = weight;
-    }
-
-    bool equals(record* r)
-    {
-        return this->id == r->id && this->weight == r->weight;
-    }
-
-    string toString()
-    {
-        return id + " " + to_string(weight);
-    }
-};
-
-vector<record> records;
+vector<pair<string, int>> records;
 
 void addRecord(string id, int weight)
 {
     auto it = records.begin();
-    while(it != records.end())
+    while (it != records.end())
     {
-        if(it->id == id && it->weight == weight) return;
-        it++;
+        if (it->first == id && it->second == weight)
+            return;
+        ++it;
     }
-    records.emplace_back(record(id, weight));
+    records.emplace_back(pair<string, int>(id, weight));
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     int a;
-    cin>>a;
-    
-    for(int i=0; i!=a; i++)
+    cin >> a;
+
+    for (int i = 0; i != a; ++i)
     {
         string id;
         int weight;
-        cin>>id>>weight;
+        cin >> id >> weight;
         addRecord(id, weight);
     }
-    
-    sort(records.begin(), records.end(), [](record a, record b){return a.id > b.id;});
-    
+
+    sort(records.begin(), records.end(), [](pair<string, int> a, pair<string, int> b)
+         { return a.first > b.first; });
+
     auto its1 = records.begin(), its2 = records.begin();
-    while(its2<records.end())
+    while (its2 != records.end())
     {
-        while((its2+1)->id == its1->id) its2++;
-        sort(its1, its2+1, [](record a, record b){return a.weight < b.weight;});
-        its2++;
-        its1=its2;
+        while ((its2 + 1)->first == its1->first)
+            ++its2;
+        sort(its1, its2 + 1, [](pair<string, int> a, pair<string, int> b)
+             { return a.second < b.second; });
+        ++its2;
+        its1 = its2;
     }
-    
-    auto it=records.begin();
-    while(it!=records.end())
+
+    auto it = records.begin();
+    while (it != records.end())
     {
-        cout<<it->toString()<<endl;
-        it++;
+        cout << it->first << " " << it->second << endl;
+        ++it;
     }
     return 0;
 }
